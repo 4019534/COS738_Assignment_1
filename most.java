72a257a3a83be89ad543ba426a2cfc43a1b50b3b
4019534 <144572947+4019534@users.noreply.github.com>
2023-10-31 04:40:24 +0200

    Submissions for COS738 Assignment 1

---
+++ most.java
@@ -0,0 +1,235 @@
+public class most{
+
+public static void main(String[] args)
+{
+    String cipherText = "GFS WMY OG LGDVS MF SFNKYHOSU ESLLMRS, PC WS BFGW POL DMFRQMRS, PL OG CPFU M UPCCSKSFO HDMPFOSXO GC OIS LMES DMFRQMRS DGFR SFGQRI OG CPDD GFS LISSO GK LG, MFU OISF WS NGQFO OIS GNNQKKSFNSL GC SMNI DSOOSK. WS NMDD OIS EGLO CKSJQSFODY GNNQKKPFR DSOOSK OIS 'CPKLO', OIS FSXO EGLO GNNQKKPFR DSOOSK OIS 'LSNGFU' OIS CGDDGWPFR EGLO GNNQKKPFR DSOOSK OIS 'OIPKU', MFU LG GF, QFOPD WS MNNGQFO CGK MDD OIS UPCCSKSFO DSOOSKL PF OIS HDMPFOSXO LMEHDS. OISF WS DGGB MO OIS NPHISK OSXO WS WMFO OG LGDVS MFU WS MDLG NDMLLPCY POL LYEAGDL. WS CPFU OIS EGLO GNNQKKPFR LYEAGD MFU NIMFRS PO OG OIS CGKE GC OIS 'CPKLO' DSOOSK GC OIS HDMPFOSXO LMEHDS, OIS FSXO EGLO NGEEGF LYEAGD PL NIMFRSU OG OIS CGKE GC OIS 'LSNGFU' DSOOSK, MFU OIS CGDDGWPFR EGLO NGEEGF LYEAGD PL NIMFRSU OG OIS CGKE GC OIS 'OIPKU' DSOOSK, MFU LG GF, QFOPD WS MNNGQFO CGK MDD LYEAGDL GC OIS NKYHOGRKME WS WMFO OG LGDVS..";
+    char first = 'a';
+    char second = 'b';
+    char third = 'c';
+    char fourth = 'd';
+    char fifth = 'e';
+    char sixth = 'f';
+    char seventh = 'g';
+    char eighth = 'h';
+    char ninth = 'i'; 
+    char tenth = 'j'; 
+    char eleventh = 'k'; 
+    char twelfth = 'l';
+    char thir = 'm'; 
+    char fourteen = 'n';  
+    char fifteen = 'o';
+    char sixteen = 'p';
+    char seventeen = 'q'; 
+    char eighteen = 'r';  
+    char ninteen = 's';
+    char twenty = 't'; 
+    char twentyone = 'u';
+    char twentytwo = 'v';
+    char twentythree = 'w';
+    char twentyfour = 'x';
+    char twentyfive = 'y'; 
+    char twentysix = 'z';  // Character to search is 'a'.
+  
+  int a=0;
+  int b=0;
+  int c=0;
+  int d=0;
+  int e=0;
+  int f=0;
+  int g=0;
+  int h=0;
+  int i=0;
+  int j=0;
+  int k=0;
+  int l=0;
+  int m=0;
+  int n=0;
+  int o=0;
+  int p=0;
+  int q=0;
+  int r=0;
+  int s=0;
+  int t=0;
+  int u=0;
+  int v=0;
+  int w=0;
+  int x=0;
+  int y=0;
+  int z=0;
+  for(int alpha=0; alpha<cipherText.length(); alpha++)
+  {
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(first))
+    //   {
+    //     a++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(second))
+    //   {
+    //     b++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(third))
+    //   {
+    //     c++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(fourth))
+    //   {
+    //     d++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(fifth))
+    //   {
+    //     e++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(sixth))
+    //   {
+    //     f++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(seventh))
+    //   {
+    //     g++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(eighth))
+    //   {
+    //     h++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(ninth))
+    //   {
+    //     alpha++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(tenth))
+    //   {
+    //     j++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(eleventh))
+    //   {
+    //     k++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twelfth))
+    //   {
+    //     l++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(thir))
+    //   {
+    //     m++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(fourteen))
+    //   {
+    //     n++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(fifteen))
+    //   {
+    //     o++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(sixteen))
+    //   {
+    //     p++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(seventeen))
+    //   {
+    //     q++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(eighteen))
+    //   {
+    //     r++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(ninteen))
+    //   {
+    //     s++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twenty))
+    //   {
+    //     t++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twentyone))
+    //   {
+    //     u++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twentytwo))
+    //   {
+    //     v++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twentythree))
+    //   {
+    //     w++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twentyfour))
+    //   {
+    //     x++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twentyfive))
+    //   {
+    //     y++;
+    //   }
+    //   if(cipherText.charAt(alpha) == Character.toUpperCase(twentysix))
+    //   {
+    //     z++;
+    //   }
+        // if(cipherText.charAt(alpha) == Character.toUpperCase(thir))
+        // {
+        //     cipherText.charAt(alpha).replace("M", "A");
+        // }
+
+    //   {
+    //     z++;
+    //   }
+    //   System.out.println("The Character '"+first+"' appears "+a+" times.");
+//   System.out.println("The Character '"+second+"' appears "+b+" times.");
+//   System.out.println("The Character '"+third+"' appears "+c+" times.");
+//   System.out.println("The Character '"+fourth+"' appears "+d+" times.");
+//   System.out.println("The Character '"+fifth+"' appears "+e+" times.");
+//   System.out.println("The Character '"+sixth+"' appears "+f+" times.");
+//   System.out.println("The Character '"+seventh+"' appears "+g+" times.");
+//   System.out.println("The Character '"+eighth+"' appears "+h+" times.");
+//   System.out.println("The Character '"+ninth+"' appears "+i+" times.");
+//   System.out.println("The Character '"+tenth+"' appears "+j+" times.");
+//   System.out.println("The Character '"+eleventh+"' appears "+k+" times.");
+//   System.out.println("The Character '"+twelfth+"' appears "+l+" times.");
+//   System.out.println("The Character '"+thir+"' appears "+m+" times.");
+//   System.out.println("The Character '"+fourteen+"' appears "+n+" times.");
+//   System.out.println("The Character '"+fifteen+"' appears "+o+" times.");
+//   System.out.println("The Character '"+sixteen+"' appears "+p+" times.");
+//   System.out.println("The Character '"+seventeen+"' appears "+q+" times.");
+//   System.out.println("The Character '"+eighteen+"' appears "+r+" times.");
+//   System.out.println("The Character '"+ninteen+"' appears "+s+" times.");
+//   System.out.println("The Character '"+twenty+"' appears "+t+" times.");
+//   System.out.println("The Character '"+twentyone+"' appears "+u+" times.");
+//   System.out.println("The Character '"+twentytwo+"' appears "+v+" times.");
+//   System.out.println("The Character '"+twentythree+"' appears "+w+" times.");
+//   System.out.println("The Character '"+twentyfour+"' appears "+x+" times.");
+//   System.out.println("The Character '"+twentyfive+"' appears "+y+" times.");
+//   System.out.println("The Character '"+twentysix+"' appears "+z+" times.");
+      
+  }
+  String try1 = cipherText.replace("M", "a");
+  String try2 = try1.replace("D", "l");
+  String try3 = try2.replace("F", "n");
+  String try4 = try3.replace("U", "d");
+  String try5 = try4.replace("G", "o");
+  String try6 = try5.replace("S", "e");
+  String try7 = try6.replace("O", "t");
+  String try8 = try7.replace("I", "h");
+  String try9 = try8.replace("K", "r");
+  String try10 = try9.replace("L", "s");
+  String try11 = try10.replace("P", "i");
+  String try12 = try11.replace("C", "f");
+  String try13 = try12.replace("E", "m");
+  String try14 = try13.replace("R", "g");
+  String try15 = try14.replace("Q", "u");
+  String try16 = try15.replace("B", "k");
+  String try17 = try16.replace("W", "w");
+  String try18 = try17.replace("N", "c");
+  String try19 = try18.replace("Y", "y");
+  String try20 = try19.replace("H", "p");
+  String try21 = try20.replace("V", "v");
+  String try22 = try21.replace("X", "x");
+  String try23 = try22.replace("J", "q");
+  String try24 = try23.replace("A", "b");
+  String decoded = try24.toUpperCase();
+
+  
+  System.out.println(try24);
+  System.out.println("");
+  System.out.println(decoded);
+
+
+}
+
+}
